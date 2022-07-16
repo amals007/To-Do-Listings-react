d055fc8cc54fb9cb87700f99445676f6cc612631
Amal S <102399747+amals007@users.noreply.github.com>
2022-07-16 19:09:41 +0530

    design

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import './App.css';
 function App() {
 
 const [toDo,setToDo] = useState([
-  { id: 1, title : 'Task1', status: false },
-  { id: 2, title : 'Task2', status: false}
+  { id: 1, title : 'Task 1', status: false },
+  { id: 2, title : 'Task 2', status: false}
 ])
 
 // Temp State
@@ -50,7 +50,74 @@ const updateTask = () => {
       <br/><br/>
         <h2>To Do List</h2>
       <br/><br/>
+
+
+
+      <div className="row">
+        <div className="col">
+          <input className="form-control form-control-lg"
+           />
+        </div>
+        <div className="col-auto">    
+          <button  
+          className="btn btn-lg btn-warning"
+          >Update</button>
+          <button  
+          className="btn btn-lg btn-warning"
+          >Cancel
+          </button>.
+        </div>
+
+      </div>
+
+
+<div className="row">
+    <div className="col">
+      <input 
+      value ={newTask}
+      onChange={ (e)=> setNewTask(e.target.value)}
+      className="form-control form-control-lg"
+      />
+    </div>
+    <div className="col-auto">
+      <button
+        onClick={addTask}
+        className="btn btn-lg btn-success"
+      >Add Task</button>
+
+    </div>
+</div>
+
+
       {toDo && toDo.length ? '':'No Tasks'}
+      {toDo && toDo
+      .sort((a, b) => a.id > b.id ? 1 : -1)
+      .map( (task,index)=>{
+        return(
+        <React.Fragment id={task.id}>
+          <div className="col taskBg">
+          <div className={task.status? 'done':''}>
+                 <span className="taskNumber">{index+1}</span>
+                 <span  className="taskText">{task.title}</span>
+
+          </div>
+          <div className="iconsWrap">
+            <span title="Completed / Not Completed">
+              <FontAwesomeIcon icon={faCircleCheck}/>
+            </span>
+            <span title="Edit">
+              <FontAwesomeIcon icon={faPen}/>
+            </span>
+            <span title="Edit">
+              <FontAwesomeIcon icon={faTrashCan}/>
+
+            </span>
+
+          </div>
+          </div>
+        </React.Fragment>
+        )
+      } )}
     </div>
   );
 }
